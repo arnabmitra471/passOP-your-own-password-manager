97c3c55b59de74c0df34f76bb6a7bb1353e22d82
Arnab Mitra <arnabmitra471@gmail.com>
2024-07-23 13:37:31 +0530

    successfully linking to the github repo for this project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,11 @@ const Navbar = () => {
         </div>
         <ul className="flex gap-5 flex-row">
           <li className="hover:bg-purple-400 py-2 px-2 hover:text-green-700 hover:rounded-md hover:transition-colors duration-200 hover:font-bold"><a href="/">Home</a></li>
-          <li className="hover:bg-purple-400 py-2 px-2 hover:text-green-700 hover:rounded-md hover:transition-colors duration-200 hover:font-bold"><a href="/">About</a></li>
           <li className="hover:bg-purple-400 py-2 px-2 hover:text-green-700 hover:rounded-md hover:transition-colors duration-200 hover:font-bold"><a href="/">Contact</a></li>
         </ul>
-        <div className="github-logo flex gap-4 justify-around items-center rounded-md bg-green-500 px-4 my-5">
+        <div className="github-logo flex gap-2 justify-around items-center rounded-full bg-green-700 px-4 my-5">
           <img src="icons/github-mark-white.png" alt="Github logo" className="w-10 p-1"/>
-          <span className="font-bold">GitHub</span>
+          <span className="font-bold"><a href="https://github.com/arnabmitra471" target="_blank">GitHub</a></span>
         </div>
       </div>
     </nav>
